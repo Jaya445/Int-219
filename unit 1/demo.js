a8fa5c1c6bd130cfe5a56554ba1f394b6da9b54e
Jaya445 <111834982+Jaya445@users.noreply.github.com>
2022-08-25 13:27:40 +0530

    Add files via upload

---
+++ unit 1/demo.js
@@ -0,0 +1,105 @@
+console.log("hello");
+let x=4;
+let y="jaya";
+let g= "Hello world";
+
+let k=5;
+
+console.log(x);
+console.log(y);
+            
+document.write("jaya");
+
+typeof x;
+typeof y;
+let z= true;
+
+console.log(55 + 66);   // Arthmatic operations
+console.log(44/22);
+console.log(x+k);
+
+
+console.log(x+y);
+console.log(typeof "jaya");
+
+x===k;   // triple equals to  used for comparison
+x>k;     // comparisons
+k>=k;
+x!=k;
+x<=k;
+j=k>=k;
+
+alert("hii i am  jaya");
+
+console.log(y.length);
+console.log(g.charAt(4));
+console.log(g.indexOf("o"));
+console.log(g.slice(5,10));
+console.log(g.toUpperCase());
+console.log(g.toLowerCase());
+console.log(g.includes("world"));
+
+//    Conversions
+
+let MyString = "44";
+let f= console.log(Number(MyString));  // converted to num  ?? Doubt
+
+
+
+// conditional statements  two - if else and switch
+const marks = 89;
+
+if (marks >= 95)
+{
+ console.log("A+");
+}
+else if(marks >=85)
+{
+console.log("A");
+}
+else if(marks >= 75)
+{
+ console.log("B");
+}
+else if (marks >= 75)
+{
+console.log("C");
+}
+else 
+{
+console.log("F");
+console.log("Fail ra bsdk");
+}
+
+
+let A,B;
+
+A= "rock";
+B="paper";
+
+if (A= "rock")
+{
+    if(B="rock")
+    {
+        console.log("Tie");
+    }
+    if(B="scissor")
+    {
+        console.log("A wins");
+    }
+    else {
+        console.log("B wins");
+    }
+}
+
+
+
+
+
+
+      
+
+
+
+
+
